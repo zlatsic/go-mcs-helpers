09e3125bcc81cc9ca3716e309673f38e13150aa5
zlatan.sicanica <zlatan.sicanica@bloomreach.com>
2023-05-21 18:22:34 +0200

    initial

---
+++ cmd/api/helpers.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+const MaxBytes = 1_000_000
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) readJSON(writer http.ResponseWriter, request *http.Request, data any) error {
+	request.Body = http.MaxBytesReader(writer, request.Body, int64(MaxBytes))
+
+	dec := json.NewDecoder(request.Body)
+
+	if err := dec.Decode(data); err != nil {
+		return err
+	}
+
+	if err := dec.Decode(&struct{}{}); err != io.EOF {
+		return errors.New("body must have only a single JSON value")
+	}
+
+	return nil
+}
+
+func (app *Config) writeJSON(writer http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			writer.Header()[key] = value
+		}
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(status)
+	_, err = writer.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (app *Config) errorJSON(writer http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJSON(writer, statusCode, payload)
+}
